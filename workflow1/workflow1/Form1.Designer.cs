d9b78d54cedaeacf56c197209f35de38b7351cc9
Alejandro Salas <145304561+AlejoSalasN@users.noreply.github.com>
2024-06-12 14:28:02 -0400

    Subiendo proyecto

---
+++ workflow1/workflow1/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace workflow1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(41, 239);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 351);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Textura";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(307, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Restaurar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkBlue;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(960, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Guardar";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(453, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(866, 456);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(453, 83);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(152, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(623, 83);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(156, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(832, 70);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 35);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Comparar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(41, 134);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(174, 134);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(307, 134);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(41, 176);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(136, 22);
+            this.textBox9.TabIndex = 15;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(272, 176);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(135, 22);
+            this.textBox10.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 602);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ workflow1/workflow1/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace workflow1
+{
+    public partial class Form1 : Form
+    {
+        int rojo = 255;
+        int verde = 255;
+        int azul = 255;
+        Bitmap originalImage;
+        bool sw = true;
+
+        int rojoOp = 0;
+        int verdeOp = 0;
+        int azulOp = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mostrar();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            sw = true;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            rojo = 255;
+            verde = 255;
+            azul = 255;
+            openFileDialog1.Filter = "Archivos de imagen|*.png;*.jpg;*.jpeg";
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName != "")
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bmp;
+                originalImage = new Bitmap(bmp);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Color c;
+            int sR;
+            int sG;
+            int sB;
+
+            int tolerance = 10; // Define una tolerancia para la comparación de colores
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    sR = c.R;
+                    sG = c.G;
+                    sB = c.B;
+
+                    // Comparar colores dentro de una tolerancia
+                    if (Math.Abs(sR - rojo) <= tolerance && Math.Abs(sG - verde) <= tolerance && Math.Abs(sB - azul) <= tolerance)
+                    {
+                        rojoOp = Math.Abs(sR - 255);
+                        verdeOp = Math.Abs(sG - 255);
+                        azulOp = Math.Abs(sB - 255);
+                        Color reemplazo = Color.FromArgb(255, rojoOp, verdeOp, azulOp);
+                        bmp2.SetPixel(i, j, reemplazo);
+                    }
+                    else
+                    {
+                        bmp2.SetPixel(i, j, c);
+                    }
+                }
+            }
+
+
+            pictureBox1.Image = bmp2;
+            
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (sw)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Color color = new Color();
+                int sR = 0;
+                int sG = 0;
+                int sB = 0;
+                //int pixelCount = 0; // Variable para contar los píxeles válidos
+
+                // Mapear las coordenadas del clic a las coordenadas de la imagen original
+                float scaleX = (float)bmp.Width / pictureBox1.ClientSize.Width;
+                float scaleY = (float)bmp.Height / pictureBox1.ClientSize.Height;
+
+                int mappedX = (int)(e.X * scaleX);
+                int mappedY = (int)(e.Y * scaleY);
+
+                color = bmp.GetPixel(mappedX, mappedY);
+                Console.WriteLine(bmp.Width + " " + bmp.Height);
+                Console.WriteLine(pictureBox1.ClientSize.Width + " " + pictureBox1.ClientSize.Height);
+                Console.WriteLine(e.X + " " + e.Y);
+                Console.WriteLine(mappedX + " " + mappedY);
+                Console.WriteLine("");
+                sR = color.R;
+                sG = color.G;
+                sB = color.B;
+
+                textBox1.Text = sR.ToString();
+                textBox2.Text = sG.ToString();
+                textBox3.Text = sB.ToString();
+                textBox6.Text = Math.Abs(sR - 255).ToString();
+                textBox7.Text = Math.Abs(sG - 255).ToString();
+                textBox8.Text = Math.Abs(sB - 255).ToString();
+                textBox10.Text = $"#{Math.Abs(sR-255):X2}{Math.Abs(sG-255):X2}{Math.Abs(sB-255):X2}";
+                textBox9.Text = $"#{sR:X2}{sG:X2}{sB:X2}";
+                rojo = int.Parse(sR.ToString());
+                verde = int.Parse(sG.ToString());
+                azul = int.Parse(sB.ToString());
+            } else
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Color color = new Color();
+                int sR = 0;
+                int sG = 0;
+                int sB = 0;
+                float scaleX = (float)bmp.Width / pictureBox1.ClientSize.Width;
+                float scaleY = (float)bmp.Height / pictureBox1.ClientSize.Height;
+
+                int mappedX = (int)(e.X * scaleX);
+                int mappedY = (int)(e.Y * scaleY);
+
+                color = bmp.GetPixel(mappedX, mappedY);
+                sR = color.R;
+                sG = color.G;
+                sB = color.B;
+
+                textBox1.Text = sR.ToString();
+                textBox2.Text = sG.ToString();
+                textBox3.Text = sB.ToString();
+                textBox6.Text = Math.Abs(sR - 255).ToString();
+                textBox7.Text = Math.Abs(sG - 255).ToString();
+                textBox8.Text = Math.Abs(sB - 255).ToString();
+                textBox10.Text = $"#{Math.Abs(sR - 255):X2}{Math.Abs(sG - 255):X2}{Math.Abs(sB - 255):X2}";
+                textBox9.Text = $"#{sR:X2}{sG:X2}{sB:X2}";
+                rojo = int.Parse(sR.ToString());
+                verde = int.Parse(sG.ToString());
+                azul = int.Parse(sB.ToString());
+
+                string query = $"SELECT color, colorN FROM texturas WHERE rojo BETWEEN {int.Parse(sR.ToString())-10} AND {int.Parse(sR.ToString()) + 10} AND verde BETWEEN {int.Parse(sG.ToString()) - 10} AND {int.Parse(sG.ToString()) + 10} AND azul BETWEEN {int.Parse(sB.ToString()) - 10} AND {int.Parse(sB.ToString()) + 10}";
+
+                try
+                {
+                    using (OdbcConnection con = new OdbcConnection("DSN=examen2"))
+                    {
+                        // Usar el DSN configurado
+                        con.Open();
+
+                        using (OdbcCommand cmd = new OdbcCommand(query, con))
+                        {
+
+                            using (OdbcDataReader reader = cmd.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    string colors = reader["color"].ToString();
+                                    string description = reader["colorN"].ToString();
+                                    textBox4.Text = colors;
+                                    textBox5.Text = description;
+                                }
+                                else
+                                {
+                                    textBox4.Text = "no hay datos";
+                                    textBox5.Text = "no hay datos";
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (OdbcException ex)
+                {
+                    MessageBox.Show("Ocurrió un error en la base de datos: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error inesperado: " + ex.Message);
+                }
+            }
+            
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rojo = 255;
+            verde = 255;
+            azul = 255;
+            if (sw)
+            {
+                if (originalImage != null)
+                {
+                    pictureBox1.Image = new Bitmap(originalImage); // Restaurar la imagen original
+                }
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rojo = 255;
+            verde = 255;
+            azul = 255;
+
+            if (sw)
+            {
+                
+                try
+                {
+                    using (OdbcConnection con = new OdbcConnection("DSN=examen2"))
+                    {
+
+                        string sR = textBox1.Text;
+                        string sG = textBox2.Text;
+                        string sB = textBox3.Text;
+
+                        // Verificar si los datos ya existen con tolerancia de 10
+                        int intSR = int.Parse(sR);
+                        int intSG = int.Parse(sG);
+                        int intSB = int.Parse(sB);
+                        con.Open();
+
+                        string checkQuery = $"SELECT COUNT(*) FROM texturas WHERE rojo BETWEEN {intSR - 10} AND {intSR + 10} AND verde BETWEEN {intSG - 10} AND {intSG + 10} AND azul BETWEEN {intSB - 10} AND {intSB + 10}";
+                        using (OdbcCommand checkCmd = new OdbcCommand(checkQuery, con))
+                        {
+                            using (OdbcDataReader reader = checkCmd.ExecuteReader())
+                            {
+                                if (reader.Read() && (reader.GetInt32(0) > 0))
+                                {
+                                    MessageBox.Show("Los datos de color ya existen en la base de datos.");
+                                    return;
+                                } else
+                                {
+                                    string insertQuery = $"INSERT INTO texturas (hexadecimal, rojo, verde, azul, color, hexadecimalN, rojoN, verdeN, azulN, colorN) VALUES ('{textBox9.Text}', '{sR}', '{sG}', '{sB}', '{textBox4.Text}', '{textBox10.Text}', '{Math.Abs(intSR-255)}', '{Math.Abs(intSG - 255)}', '{Math.Abs(intSB - 255)}', '{textBox5.Text}')";
+
+                                    using (OdbcCommand insertCmd = new OdbcCommand(insertQuery, con))
+                                    {
+                                        insertCmd.ExecuteNonQuery();
+                                        MessageBox.Show("Datos insertados correctamente.");
+                                        mostrar(); // Actualizar la vista
+                                    }
+                                }
+                            }
+                        }
+
+                        //string insertQuery = $"INSERT INTO texturas (descripcion, cR, cG, cB, color) VALUES ({textBox4.Text}, {sR}, {sG}, {sB}, {textBox5.Text})";
+
+                        con.Close();
+                    }
+                }
+                catch (OdbcException ex)
+                {
+                    MessageBox.Show("Ocurrió un error en la base de datos: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error inesperado: " + ex.Message);
+                }
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+                textBox7.Text = "";
+                textBox8.Text = "";
+                textBox9.Text = "";
+                textBox10.Text = "";
+            } else
+            {
+                Console.WriteLine("juasjuas");
+            }
+        }
+
+        public void mostrar()
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection())
+                {
+                    // Usar el DSN configurado
+                    con.ConnectionString = "DSN=examen2";
+
+                    using (OdbcDataAdapter ada = new OdbcDataAdapter())
+                    {
+                        ada.SelectCommand = new OdbcCommand();
+                        ada.SelectCommand.Connection = con;
+                        ada.SelectCommand.CommandText = "SELECT hexadecimal, rojo, verde, azul, color, hexadecimalN, rojoN, verdeN, azulN, colorN FROM texturas";
+                        DataSet ds = new DataSet();
+                        ada.Fill(ds);
+                        dataGridView1.DataSource = ds.Tables[0];
+                    }
+                }
+            }
+            catch (OdbcException ex)
+            {
+                MessageBox.Show("Ocurrió un error en la base de datos: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error inesperado: " + ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sw = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            rojo = 255;
+            verde = 255;
+            azul = 255;
+            openFileDialog1.Filter = "Archivos de imagen|*.png;*.jpg;*.jpeg";
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName != "")
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bmp;
+                originalImage = new Bitmap(bmp);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
